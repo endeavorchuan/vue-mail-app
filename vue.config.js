4361778bd0766943325ab9cf5e326ca9025a9ba0
川 <chc82347794@163.com>
2022-03-31 20:04:24 +0800

    项目初始化：lib-flexible、px2m、vant

---
+++ vue.config.js
@@ -0,0 +1,23 @@
+/**
+ * @name: vue.config
+ * @author: chuanchuan
+ * @date: 2022-03-31 16:26
+ * @description：vue.config
+ * @update: 2022-03-31 16:26
+ */
+
+const px2rem = require('postcss-px2rem');
+
+module.exports = {
+  css: {
+    loaderOptions: {
+      postcss: {
+        plugins: [
+          px2rem({
+            remUnit: 75,
+          }),
+        ],
+      },
+    },
+  },
+};
